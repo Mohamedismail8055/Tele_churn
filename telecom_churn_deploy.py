a433223996766ebd7c50781a1cadfb8efacba1e4
Mohamedismail8055 <141264013+Mohamedismail8055@users.noreply.github.com>
2025-03-01 13:43:49 +0530

    Update telecom_churn_deploy.py

--- telecom_churn_deploy.py
+++ telecom_churn_deploy.py
@@ -16,10 +16,6 @@ subprocess.check_call(['pip', 'install', '-r', 'requirements.txt'])
 
 """## Importing Libraries"""
 
-import subprocess
-subprocess.run(["pip", "install", "imbalanced-learn"], check=True)
-
-from imblearn.over_sampling import SMOTE
 
 import numpy as np
 import pandas as pd
